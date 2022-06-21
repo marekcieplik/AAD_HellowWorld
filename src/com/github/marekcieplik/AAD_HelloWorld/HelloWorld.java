2cc951c2d43a32de830abc7ad0d56d5c3a7b394e
marekcieplik <marek.cieplik@gmail.com>
2022-06-21 10:59:00 +0200

    Congratulations: This is your very first application! :)

---
+++ src/com/github/marekcieplik/AAD_HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package src.com.github.marekcieplik.AAD_HelloWorld;
+
+public class HelloWorld { //class declaration don't need any semi-colon
+    public static void main(String[] args) {  //every application need main method
+        System.out.println("Hello World!");
+        System.out.println("Congratulations: This is your very first application! :)");
+    }
+}
